59d473bab777735890e5019b5f4afada68b4f6e2
Mykola Zaikovskyi <mykola.zaikovskyi@gmail.com>
2023-04-04 13:37:54 +0300

    fix import RecipePage

--- src/pages/RecipePage/RecipePage.jsx
+++ src/pages/RecipePage/RecipePage.jsx
@@ -1,5 +1,18 @@
+// import { useParams } from 'react-router-dom';
+
+import MainContainer from 'components/MainContainer/MainContainer';
+
 const RecipePage = () => {
-  return <div>RecipePage</div>;
+  // потрібно взяти айді рецепту з рядка запиту через хук useParams
+  // const { recipeId } = useParams();
+
+  // потрібно зробити запит в глобальний стейт
+
+  return (
+    <MainContainer>
+      <div>RecipePage</div>;
+    </MainContainer>
+  );
 };
 
 export default RecipePage;
