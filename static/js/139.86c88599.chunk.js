5f57ddcfd90277364538563c4504d8ad8394b2c2
Bohdan100 <104660586+Bohdan100@users.noreply.github.com>
2023-04-05 21:27:43 +0000

    Deploying to gh-pages from @ Bohdan100/so-yummy-frontend@a262ee8dc107c0f79a8ce294269131038092ccb2 🚀

--- static/js/139.86c88599.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkso_yummy_frontend=self.webpackChunkso_yummy_frontend||[]).push([[139],{6139:function(n,e,u){u.r(e),u.d(e,{default:function(){return t}});var r=u(184),t=function(){return(0,r.jsx)("div",{children:"MyRecipesPage"})}}}]);
-//# sourceMappingURL=139.86c88599.chunk.js.map
