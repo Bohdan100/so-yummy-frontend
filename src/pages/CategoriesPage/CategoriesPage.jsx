24247bae14808de0c589457857e1516ddafbff87
olhazamlynska <olya.sonechko@gmail.com>
2023-04-05 20:40:59 +0300

    change route

--- src/pages/CategoriesPage/CategoriesPage.jsx
+++ src/pages/CategoriesPage/CategoriesPage.jsx
@@ -1,18 +1,15 @@
-import Loader from 'components/Loader/Loader';
 import Container from '../../components/MainContainer/MainContainer';
 import CategoriesList from 'components/CategoriesList/CategoriesList';
 import ReusableTitle from 'components/ReusableTitle/ReusableTitle';
-import { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
+import CategoriesByName from 'components/CategoriesByName';
+
 const CategoriesPage = () => {
   return (
     <div>
       <Container>
         <ReusableTitle>Categories</ReusableTitle>
         <CategoriesList />
-        <Suspense fallback={<Loader />}>
-          <Outlet />
-        </Suspense>
+        <CategoriesByName />
       </Container>
     </div>
   );
