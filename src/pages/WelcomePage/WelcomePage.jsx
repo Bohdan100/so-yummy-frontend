2b7454a6adbab1843d35ac1c4e596e0e13c42f04
VLADYSLAV FEDCHENKO <vladfedcenko33@gmail.com>
2023-04-06 01:38:10 +0300

    Create WelcomePage + favicon

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,5 +1,41 @@
+import {
+  LinkRegister,
+  LinkLogin,
+  Container,
+  TextBox,
+  Title,
+  Text,
+  BoxLink,
+  Box,
+  Image,
+} from './WelcomePage.styled';
+
+import MainContainer from '../../components/MainContainer/MainContainer';
+
+import Logo from '../../images/icons/logo-desktop.svg';
+
 const WelcomePage = () => {
-  return <div>WelcomePage</div>;
+  return (
+    <MainContainer>
+      <Container>
+        <Box>
+          <Image src={Logo} width={68} height={68} />
+          <TextBox>
+            <Title>Welcome to the app!</Title>
+            <Text>
+              This app offers more than just a collection of recipes - it is
+              designed to be your very own digital cookbook. You can easily save
+              and retrieve your own recipes at any time.
+            </Text>
+          </TextBox>
+          <BoxLink>
+            <LinkRegister to="/register">Registration</LinkRegister>
+            <LinkLogin to="/signin">Sign in</LinkLogin>
+          </BoxLink>
+        </Box>
+      </Container>
+    </MainContainer>
+  );
 };
 
 export default WelcomePage;
