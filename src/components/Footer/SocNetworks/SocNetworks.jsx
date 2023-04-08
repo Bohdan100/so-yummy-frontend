e831af43f32edc35524d3db0654a3ad1e4c3a969
Anna Lisitsyna <anna.lisitsyna93@gmail.com>
2023-04-08 16:48:49 +0300

    fix footer

--- src/components/Footer/SocNetworks/SocNetworks.jsx
+++ src/components/Footer/SocNetworks/SocNetworks.jsx
@@ -48,49 +48,4 @@ const SocNetworks = ({ location }) => {
   );
 };
 
-// const SocNetworks = () => {
-//   return (
-//     <SocNetworksWraper>
-//       <SocNetworksList>
-//         <SocNetwoerksItem>
-//           <Link
-//             href="https://www.facebook.com/"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             <FacebookStyled width="20" height="20" />
-//           </Link>
-//         </SocNetwoerksItem>
-//         <SocNetwoerksItem>
-//           <Link
-// href="https://www.youtube.com/"
-// target="_blank"
-// rel="noopener noreferrer"
-//           >
-//             <YoutubeStyled width="20" height="15" />
-//           </Link>
-//         </SocNetwoerksItem>
-//         <SocNetwoerksItem>
-//           <Link
-// href="https://www.twitter.com/"
-// target="_blank"
-// rel="noopener noreferrer"
-//           >
-//             <TwitterStyled width="20" height="16" />
-//           </Link>
-//         </SocNetwoerksItem>
-//         <SocNetwoerksItem>
-//           <Link
-// href="https://www.twitter.com/"
-// target="_blank"
-// rel="noopener noreferrer"
-//           >
-//             <InstagramStyled width="20" height="20" />
-//           </Link>
-//         </SocNetwoerksItem>
-//       </SocNetworksList>
-//     </SocNetworksWraper>
-//   );
-// };
-
 export default SocNetworks;
--- src/components/Footer/SocNetworks/SocNetworks.styled.js
+++ src/components/Footer/SocNetworks/SocNetworks.styled.js
@@ -1,10 +1,5 @@
 import styled from 'styled-components';
 
-import { ReactComponent as Facebook } from '../../../images/icons/facebook.svg';
-import { ReactComponent as Youtube } from '../../../images/icons/youtube.svg';
-import { ReactComponent as Twitter } from '../../../images/icons/twitter.svg';
-import { ReactComponent as Instagram } from '../../../images/icons/instagram.svg';
-
 export const SocLinkList = styled.ul`
   display: flex;
   gap: 16px;
