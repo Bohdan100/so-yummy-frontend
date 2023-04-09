b4f3143219ab77d2a3cd3da6aace2c8ad1631133
Bohdan <104660586+Bohdan100@users.noreply.github.com>
2023-04-09 03:08:03 +0300

    fixed

--- src/components/IngredientsShoppingItem/IngredientsShoppingItem.styled.js
+++ src/components/IngredientsShoppingItem/IngredientsShoppingItem.styled.js
@@ -1,24 +1,26 @@
 import styled from 'styled-components';
 
-import { ReactComponent as PickIcon } from '../../images/icons/pick.svg';
+import { ReactComponent as PickIcon } from '../../images/icons/close-20.svg';
 
-export const RecipeItem = styled.li`
+export const IngredientItem = styled.li`
   border-radius: ${li => li.theme.radii.image};
   background-color: ${li => li.theme.colors.secondaryAccent};
+  border-bottom: 2px solid rgba(126, 126, 126, 0.5);
 `;
 
-export const RecipeItemWrapper = styled.label`
+export const IngredientItemWrapper = styled.label`
   display: flex;
-  align-items: center;
-  padding: 14px 29px 14px 14px;
+  align-items: start;
+  padding: 29px 22px;
+  background: ${p => p.theme.colors.mainWhite};
   &:hover {
     cursor: pointer;
   }
-  @media screen and (min-width: 768px) {
-    padding: 21px 58px 21px 28px;
+  @media screen and (${p => p.theme.breakpoints[1]}) {
+    padding: 32px 42px;
   }
-  @media screen and (min-width: 1440px) {
-    padding: 5px 70px 5px 36px;
+  @media screen and (${p => p.theme.breakpoints[2]}) {
+    padding: 50px 146px;
   }
 `;
 
@@ -26,26 +28,26 @@ export const ImageWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-right: 8px;
+  margin-right: 10px;
 
   @media screen and (min-width: ${p => p.theme.breakpoints[1]}) {
-    margin-right: 24px;
+    margin-right: 16px;
   }
   @media screen and (min-width: ${p => p.theme.breakpoints[2]}) {
-    margin-right: 40px;
+    margin-right: 16px;
   }
 
   & img {
     border-radius: ${p => p.theme.radii.image};
-    width: 57px;
-    height: 57px;
+    width: 48px;
+    height: 48px;
     @media screen and (min-width: ${p => p.theme.breakpoints[1]}) {
-      width: 136px;
-      height: 136px;
+      width: 81px;
+      height: 81px;
     }
     @media screen and (min-width: ${p => p.theme.breakpoints[2]}) {
-      width: 172px;
-      height: 172px;
+      width: 120px;
+      height: 120px;
     }
   }
 `;
@@ -57,6 +59,7 @@ export const TextContainer = styled.div`
   display: -webkit-box;
   -webkit-line-clamp: 4;
   -webkit-box-orient: vertical;
+
   @media screen and (min-width: ${p => p.theme.breakpoints[1]}) {
     max-width: 208px;
   }
@@ -67,28 +70,13 @@ export const TextContainer = styled.div`
 
 export const NameIngredient = styled.p`
   font-weight: ${p => p.theme.fontWeights[1]};
-  font-size: 12px;
-  line-height: 1.17;
+  font-size: 10px;
+  line-height: ${p => p.theme.lineHeights.btnText};
   letter-spacing: ${p => p.theme.letterSpacings.subheader};
-  color: ${li => li.theme.colors.secondaryDark};
+  color: ${p => p.theme.colors.mainDark};
   @media screen and (min-width: ${p => p.theme.breakpoints[1]}) {
-    font-size: 24px;
-    line-height: ${p => p.theme.lineHeights.subheader};
-  }
-`;
-
-export const DescriptionIngridient = styled.p`
-  font-weight: ${p => p.theme.fontWeights[0]};
-  font-size: 8px;
-  line-height: 1.38;
-  letter-spacing: ${p => p.theme.letterSpacings.subheader};
-  color: ${p => p.theme.colors.mainGrey};
-  @media screen and (min-width: ${p => p.theme.breakpoints[1]}) {
-    overflow-y: clip;
-
-    line-height: ${p => p.theme.lineHeights.subheader};
-    font-size: 14px;
-    line-height: 1.67;
+    font-size: 16px;
+    line-height: ${p => p.theme.lineHeights.time};
   }
 `;
 
@@ -105,28 +93,28 @@ export const WeighIngredient = styled.p`
   background: ${p => p.theme.colors.mainAccent};
   font-weight: ${p => p.theme.fontWeights[2]};
   font-size: 10px;
-  line-height: 1.5;
-  color: ${p => p.theme.colors.mainLight};
-  @media screen and (min-width: 768px) {
+  line-height: ${p => p.theme.lineHeights.time};
+  color: ${p => p.theme.colors.mainWhite};
+  @media screen and (min-width: ${p => p.theme.breakpoints[1]}) {
     min-width: 68px;
     max-width: 150px;
     font-size: 18px;
-    line-height: 1.5;
+  }
+  @media screen and (min-width: ${p => p.theme.breakpoints[2]}) {
+    min-height: 35px;
   }
 `;
 
-export const CustomCheckbox = styled.span`
+export const DeleteButton = styled.button`
   margin-left: 39px;
   position: relative;
   display: inline-block;
   width: 18px;
   height: 18px;
-  background: transparent;
-  border: 1px solid rgba(126, 126, 126, 0.5);
-  border-radius: ${p => p.theme.radii.btnIcon};
+  background: ${p => p.theme.colors.mainWhite};
+  border-color: transparent;
 
   @media screen and (min-width: ${p => p.theme.breakpoints[1]}) {
-    border: 2px solid rgba(126, 126, 126, 0.5);
     width: 35px;
     height: 35px;
     margin-left: 78px;
@@ -140,27 +128,13 @@ export const PickIconStyled = styled(PickIcon)`
   display: inline-block;
   width: 9px;
   height: 9px;
+  color: ${p => p.theme.colors.mainWhite};
   position: absolute;
   left: 50%;
   top: 50%;
-  transform: translate(-50%, -50%) scale(0);
+  transform: translate(-50%, -50%);
   @media screen and (min-width: ${p => p.theme.breakpoints[1]}) {
     width: 17px;
     height: 17px;
-  }
-`;
-
-export const DeleteButton = styled.button`
-  padding: 8px 16px;
-  font-size: 16px;
-  font-weight: 500;
-  line-height: 24px;
-  color: #ffffff;
-  background-color: #eb5757;
-  border-radius: 8px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: #cc4c4c;
   }
 `;
--- src/pages/ShoppingListPage/ShoppingListPage.styled.js
+++ src/pages/ShoppingListPage/ShoppingListPage.styled.js
@@ -5,6 +5,12 @@ export const ShoppingListPageWrapper = styled.div`
   flex-direction: column;
   align-items: center;
   padding-top: 64px;
+
+  margin-left: 16px;
+  @media (min-width: ${p => p.theme.breakpoints[1]}) {
+  }
+  @media (min-width: ${p => p.theme.breakpoints[2]}) {
+  }
 `;
 
 export const ShopListTitle = styled.h1`
@@ -12,11 +18,17 @@ export const ShopListTitle = styled.h1`
   font-style: normal;
   font-weight: 600;
   font-size: 28px;
+  line-height: ${p => p.theme.lineHeights.subheader};
   color: #001833;
-  @media (min-width: 768px) {
+  margin-top: 50px;
+  margin-bottom: 50px;
+
+  @media (min-width: ${p => p.theme.breakpoints[1]}) {
     font-size: 32px;
+    margin-top: 85px;
+    margin-bottom: 72px;
   }
-  @media (min-width: 1440px) {
+  @media (min-width: ${p => p.theme.breakpoints[2]}) {
     font-size: 44px;
   }
 `;
@@ -41,24 +53,23 @@ export const HeaderTable = styled.div`
   justify-content: space-between;
   border-radius: ${div => div.theme.radii.image};
   height: 42px;
-  padding: 0 14px;
-  margin-bottom: 24px;
-  color: ${p => p.theme.colors.mainLight};
+  padding: 10px;
+  margin-bottom: 32px;
+  color: ${p => p.theme.colors.mainWhite};
   background-color: ${p => p.theme.colors.mainAccent};
-  @media screen and (min-width: 768px) {
+  @media screen and (min-width: ${p => p.theme.breakpoints[1]}) {
     height: 58px;
-    padding: 0 32px;
-    margin-bottom: 32px;
-  }
-  @media screen and (min-width: 1440px) {
-    height: 60px;
-    padding: 0 40px;
+    padding: 20px;
     margin-bottom: 50px;
+  }
+  @media screen and (min-width: ${p => p.theme.breakpoints[2]}) {
+    height: 60px;
+    padding: 20px 40px;
   }
   p {
     font-weight: ${p => p.theme.fontWeights[2]};
-    font-size: 10px;
-    line-height: 1.5;
+    font-size: 12px;
+    line-height: ${p => p.theme.lineHeights.time};
     letter-spacing: 0.03em;
     @media screen and (min-width: ${p => p.theme.breakpoints[1]}) {
       font-size: 18px;
@@ -70,7 +81,8 @@ export const HeaderTable = styled.div`
       margin-left: 38px;
     }
     @media screen and (min-width: ${p => p.theme.breakpoints[2]}) {
-      margin-left: 109px;
+      margin-left: 110px;
     }
   }
 `;
+// 24 78 142
