96253adcb1ecaf7f3daebdacd49241795df482d7
Mykola Zaikovskyi <mykola.zaikovskyi@gmail.com>
2023-04-09 12:28:34 +0300

    fix paddig

--- src/pages/RecipePage/RecipePage.styled.js
+++ src/pages/RecipePage/RecipePage.styled.js
@@ -1,6 +1,15 @@
 import styled from 'styled-components';
 
-export const RecipePageStyled = styled.div``;
+export const RecipePageStyled = styled.div`
+  padding-top: 426px;
+
+  @media screen and (min-width: ${p => p.theme.breakpoints[1]}) {
+    padding-top: 482px;
+  }
+  @media screen and (min-width: ${p => p.theme.breakpoints[2]}) {
+    padding-top: 480px;
+  }
+`;
 
 export const HeaderTable = styled.div`
   display: flex;
@@ -9,17 +18,19 @@ export const HeaderTable = styled.div`
   border-radius: ${div => div.theme.radii.image};
   height: 42px;
   padding: 0 14px;
+
   margin-bottom: 24px;
   color: ${p => p.theme.colors.mainLight};
   background-color: ${p => p.theme.colors.mainAccent};
-  @media screen and (min-width: 768px) {
+  @media screen and (min-width: ${p => p.theme.breakpoints[1]}) {
     height: 58px;
     padding: 0 32px;
     margin-bottom: 32px;
   }
-  @media screen and (min-width: 1440px) {
+  @media screen and (min-width: ${p => p.theme.breakpoints[2]}) {
     height: 60px;
     padding: 0 40px;
+
     margin-bottom: 50px;
   }
   p {
