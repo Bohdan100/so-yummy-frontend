252ff171786fdbb41374d4d838f232e1241be1cc
olhazamlynska <olya.sonechko@gmail.com>
2023-04-09 21:52:03 +0300

    add dots

--- src/components/ReusableComponents/ReusableTitle/ReusableTitle.jsx
+++ src/components/ReusableComponents/ReusableTitle/ReusableTitle.jsx
@@ -1,7 +1,13 @@
+import BackgroundDots from '../BackgroundDots/BackgroundDots';
 import { Title } from './ReusableTitle.styled';
 
 const ReusableTitle = ({ children }) => {
-  return <Title>{children}</Title>;
+  return (
+    <>
+      <BackgroundDots />
+      <Title>{children}</Title>
+    </>
+  );
 };
 
 export default ReusableTitle;
