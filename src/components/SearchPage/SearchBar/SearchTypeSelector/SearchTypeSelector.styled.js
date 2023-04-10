a5b549c9f164a9c4542fc293ba30dabe2b5c25af
Scumsy <dmitry.koschavka@gmail.com>
2023-04-10 13:15:45 +0300

    conflict resolve

---
+++ src/components/SearchPage/SearchBar/SearchTypeSelector/SearchTypeSelector.styled.js
@@ -0,0 +1,127 @@
+import styled from 'styled-components';
+import Select from 'react-select';
+
+export const SelectCon = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 15px;
+  text-align: center;
+  span {
+    font-style: normal;
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 16px;
+    letter-spacing: -0.24px;
+    color: ${p => p.theme.colors.sectionHeader};
+    @media screen and (min-width: 768px) {
+      font-size: 14px;
+      line-height: 18px;
+    }
+    @media screen and (min-width: 1440px) {
+      font-size: 18px;
+      line-height: 24px;
+    }
+  }
+`;
+
+export const SelectStyled = styled(Select)`
+  & .react-select__control {
+    background-color: ${p => p.theme.colors.searchSelectBg};
+    border: ${p => p.theme.borders.searchInput};
+    outline: none;
+    box-shadow: none;
+    border-radius: ${p => p.theme.radii.searchSelectBg};
+    width: 146px;
+    height: 34px;
+    font-size: 12px;
+    line-height: 18px;
+    letter-spacing: ${p => p.theme.letterSpacings.content};
+    color: ${p => p.theme.colors.searchSelectText};
+    &--is-focused,
+    &--menu-is-open {
+      outline: none;
+      border: 1px solid transparent;
+    }
+    &:focus,
+    &:active,
+    &:hover,
+    &::selection {
+      outline: none;
+      border-color: transparent;
+      box-shadow: 0 0 0 1px transparent;
+    }
+    @media (min-width: 768px) {
+      font-size: 14px;
+      line-height: 21px;
+      width: 175px;
+      height: 41px;
+    }
+    & .react-select__value-container {
+      height: 100%;
+      display: flex;
+      align-items: center;
+      & .react-select__single-value {
+        display: flex;
+        align-items: center;
+        height: 21px;
+      }
+    }
+  }
+  & .react-select__single-value {
+    font-family: ${p => p.theme.fonts.main};
+    font-weight: ${p => p.theme.fontWeights[0]};
+    text-align: left;
+    font-size: 12px;
+    line-height: 18px;
+    letter-spacing: ${p => p.theme.letterSpacings.content};
+    color: ${p => p.theme.colors.searchSelectText};
+    @media (min-width: 768px) {
+      font-size: 14px;
+      line-height: 21px;
+    }
+  }
+  & .react-select-contanier {
+    box-shadow: none;
+    outline: none;
+    border-color: transparent;
+    &:focus,
+    &:active,
+    &:hover,
+    &::selection {
+      outline: none;
+      border-color: transparent;
+      box-shadow: 0 0 0 1px transparent;
+    }
+  }
+  & .react-select__menu-list {
+    background-color: ${p => p.theme.colors.searchDropDownBg};
+    border-radius: 0px 0px 6px 6px;
+    ${p => p.theme.letterSpacings.content};
+    font-size: 12px;
+    line-height: 18px;
+    text-align: left;
+    @media (min-width: 768px) {
+      font-size: 14px;
+      line-height: 21px;
+    }
+    color: rgba(0, 0, 0, 0.5) !important;
+  }
+  & .react-select__option {
+    background-color: ${p => p.theme.colors.searchDropDownBg};
+    color: rgba(0, 0, 0, 0.5);
+  }
+  & .react-select__dropdown-indicator {
+    color: ${p => p.theme.colors.mainAccent};
+  }
+  & .react-select__indicator-separator {
+    display: none;
+  }
+  & .react-select__menu {
+    margin: 0;
+    border-radius: 0px 0px 6px 6px;
+  }
+  & .react-select__placeholder {
+    color: rgba(0, 0, 0, 0.5);
+  }
+`;
--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,5 +1,79 @@
+import React, { useEffect, useState } from 'react';
+import MainContainer from 'components/MainContainer/MainContainer';
+import ReusableTitle from 'components/ReusableComponents/ReusableTitle/ReusableTitle';
+import { SearchBar } from 'components/SearchPage/SearchBar/SearchBar';
+import { useSearchParams } from 'react-router-dom';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import { NoSearchResult } from 'components/SearchPage/NoSearchResult/NoSearchResult';
+
+import NotFoundWrapp from 'components/CategoriesByName/NotFoundWrapp';
+import Loader from 'components/Loader/Loader';
+
 const SearchPage = () => {
-  return <div>SearchPage</div>;
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [recipes, setRecipes] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get('query') ?? '';
+  const type = searchParams.get('type') ?? 'Title';
+
+  const onSubmit = (query, type) => {
+    let page = 1;
+    let perPage = 5;
+    setSearchParams({ type, query, page, perPage });
+  };
+
+  async function FetchSearchedMeals(searchParams) {
+    const response = await axios.get(`/search`, { params: searchParams });
+
+    console.log(response.data.meals);
+    return response.data.meals;
+  }
+
+  useEffect(() => {
+    if (query === '' || type === '') return;
+
+    async function SearchRecipes() {
+      try {
+        setIsLoading(true);
+        const recipes = await FetchSearchedMeals(searchParams);
+        console.log(recipes);
+        if (recipes.length === 0) {
+          toast.error(
+            'Sorry, there are no recipes matching your search query. Please, try again.',
+            {
+              position: 'top-right',
+            }
+          );
+        }
+        setRecipes(recipes);
+      } catch (error) {
+        toast.error('Something went wrong. Please, reload the page.', {
+          position: 'top-center',
+        });
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    SearchRecipes();
+  }, [type, query]);
+
+  return (
+    <MainContainer>
+      <ReusableTitle>Search</ReusableTitle>
+      <SearchBar onSubmit={onSubmit} startType={type} startQuery={query} />
+      {isLoading ? (
+        <Loader />
+      ) : recipes < 1 ? (
+        <NotFoundWrapp>Try looking for something else..</NotFoundWrapp>
+      ) : (
+        <p>Recepies will be here</p>
+      )}
+    </MainContainer>
+  );
 };
 
 export default SearchPage;
