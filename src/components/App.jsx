c80177351963045bc0c46dd78af2b8cd39af7609
Anna Lisitsyna <anna.lisitsyna93@gmail.com>
2023-04-13 21:54:57 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { refresh } from 'redux/Auth/authOperations';
 import { selectTheme } from 'redux/Theme/themeSelectors';
 import { selectIsRefreshing } from 'redux/Auth/authSelectors';
-import { fetchProducts } from '../redux/ShoppingList/shoppingListOperations';
 
 import Loader from 'components/Loader/Loader';
 import { ToastContainer } from 'react-toastify';
@@ -43,7 +42,6 @@ export const App = () => {
 
   useEffect(() => {
     dispatch(refresh());
-    dispatch(fetchProducts());
   }, [dispatch]);
 
   return isRefreshUser ? (
