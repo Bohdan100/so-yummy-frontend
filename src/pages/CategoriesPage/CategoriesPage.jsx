6b6bab89b828aed4459b92c7921a13f2dab8bde7
Bohdan <104660586+Bohdan100@users.noreply.github.com>
2023-04-16 19:56:54 +0300

    scrool

--- src/pages/CategoriesPage/CategoriesPage.jsx
+++ src/pages/CategoriesPage/CategoriesPage.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
@@ -5,9 +6,14 @@ import MainContainer from '../../components/MainContainer';
 import CategoriesList from 'components/CategoriesByName/CategoriesList/CategoriesList';
 import ReusableTitle from 'components/ReusableComponents/ReusableTitle/ReusableTitle';
 import Loader from 'components/Loader/Loader';
+import { scrollToTop } from 'helpers/scrollToTop';
 
 const CategoriesPage = () => {
   const { t } = useTranslation();
+
+  useEffect(() => {
+    scrollToTop();
+  }, []);
 
   return (
     <MainContainer>
